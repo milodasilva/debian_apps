74114e4786092dca6f1ee540852ea4c2d45c3b59
Milo da Silva <milodasp@gmail.com>
2023-09-02 20:06:28 -0300

    orgalized file

--- after-install.sh
+++ after-install.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-sudo apt install	xorg \
-									pcmanfm \
-									xarchiver \
-									lxterminal \
-									xfce4-screenshooter \
-									xinput \
-									pulseaudio \
-									pavucontrol \
-									lxappearance \
-									xclip \
-									arc-theme \
-									gcc \
-									g++ \
-									make \
-									software-properties-common \
-									build-essential \
-									ca-certificates \
-									network-manager \ 
-									htop \
-									curl 
+sudo apt install xorg \
+pcmanfm \
+xarchiver \
+lxterminal \
+xfce4-screenshooter \
+xinput \
+pulseaudio \
+pavucontrol \
+lxappearance \
+xclip \
+arc-theme \
+gcc \
+g++ \
+make \
+software-properties-common \
+build-essential \
+ca-certificates \
+network-manager \ 
+htop \
+curl 
